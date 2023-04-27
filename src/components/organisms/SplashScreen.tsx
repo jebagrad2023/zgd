1c1275b247023b6e77cc67151e7718a51b68d9d2
Doramanjyu <24859285+Doramanjyu@users.noreply.github.com>
2023-04-27 19:49:06 +0900

    Add splash screen

---
+++ src/components/organisms/SplashScreen.tsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react'
+
+type Props = {
+  onDone: () => void
+}
+
+const progressPattern = [1, 1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 8, 9]
+
+export const SplashScreen = ({ onDone }: Props): JSX.Element => {
+  const [progress, setProgress] = useState(1)
+
+  useEffect(() => {
+    const timer = setInterval(() => setProgress((p) => p + 1), 150)
+    return () => clearInterval(timer)
+  }, [])
+
+  useEffect(() => {
+    if (progress === 15) {
+      onDone()
+    }
+  }, [progress])
+
+  const bar =
+    progress < progressPattern.length
+      ? progressPattern[progress]
+      : progressPattern[progressPattern.length - 1]
+
+  return (
+    <div id="splashScreen">
+      <div id="beamsLogo">
+        {Array.from(Array(bar).keys()).map((i) => (
+          <div className="beamsProgress" key={i} />
+        ))}
+      </div>
+    </div>
+  )
+}
