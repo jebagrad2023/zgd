2a715746d6f0afbf08a1f12731b0d32e1129da39
Doramanjyu <24859285+Doramanjyu@users.noreply.github.com>
2023-05-11 21:56:33 +0900

    Add ZEA ghost passing by

--- src/components/organisms/Grave.tsx
+++ src/components/organisms/Grave.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
 import MemorialFrame from '@zgd/images/zeaframewiggle1.gif'
+import zeaGhost from '@zgd/images/zea_ghost.png'
 
-export const Grave = (): JSX.Element => (
-  <>
+export const Grave = (): JSX.Element => {
+  const ghost = useRef<HTMLImageElement>()
+
+  useEffect(() => {
+    const startPassing = () => {
+      if (!ghost.current) {
+        return
+      }
+      const onAnimationEnd = () => {
+        ghost.current.style.display = 'none'
+      }
+      ghost.current.style.top = `${Math.random() * 80}vh`
+      ghost.current.style.display = 'block'
+      ghost.current.addEventListener('animationend', onAnimationEnd)
+    }
+    const wait = () => 30000 + Math.floor(Math.random() * 60000)
+
+    let timer: ReturnType<typeof setTimeout>
+    const trigger = () => {
+      startPassing()
+      timer = setTimeout(trigger, wait())
+    }
+    timer = setTimeout(trigger, wait())
+
+    return () => clearTimeout(timer)
+  }, [ghost])
+
+  return (
     <div className="graveBG">
       <div className="graveContents">
         <div className="topMemorialTitle">
@@ -13,6 +40,9 @@ export const Grave = (): JSX.Element => (
         <img src={MemorialFrame} className="frame" /> <br />
         <div className="bottomMemorialTitle">2019 - 2023</div>
       </div>
+      <div id="zeaGhost" ref={ghost}>
+        <img src={zeaGhost} />
+      </div>
     </div>
-  </>
-)
+  )
+}
