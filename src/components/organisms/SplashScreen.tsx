e88cbd83685dbec92bae7748ba56e92e1359952e
Doramanjyu <24859285+Doramanjyu@users.noreply.github.com>
2023-05-12 20:18:48 +0900

    Add BEA frame to splash screen on large screen (#101)

--- src/components/organisms/SplashScreen.tsx
+++ src/components/organisms/SplashScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 import beamsProgress from '@zgd/images/beams_progress.png'
 import beamsLogo from '@zgd/images/beams.png'
@@ -11,6 +11,7 @@ const progressPattern = [1, 1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 8, 9]
 
 export const SplashScreen = ({ onDone }: Props): JSX.Element => {
   const [progress, setProgress] = useState(1)
+  const outerScreen = useRef<HTMLDivElement>()
 
   useEffect(() => {
     const img = new Image()
@@ -25,8 +26,10 @@ export const SplashScreen = ({ onDone }: Props): JSX.Element => {
       }
     }
   }, [])
-
   useEffect(() => {
+    if (progress === progressPattern.length - 6) {
+      outerScreen.current.classList.add('zoomOut')
+    }
     if (progress === progressPattern.length + 4) {
       onDone()
     }
@@ -38,18 +41,29 @@ export const SplashScreen = ({ onDone }: Props): JSX.Element => {
       : progressPattern[progressPattern.length - 1]
 
   return (
-    <div
-      id="splashScreen"
-      className={progress > progressPattern.length ? 'transitioning' : ''}
-      onClick={onDone}
-    >
-      <div id="beamsLogo">
-        <div id="beamsProgressArea">
-          {Array.from(Array(bar).keys()).map((i) => (
-            <img src={beamsProgress} className="beamsProgress" key={i} />
-          ))}
+    <div id="splashOuterScreen" ref={outerScreen}>
+      <div
+        id="splashScreen"
+        className={progress > progressPattern.length ? 'transitioning' : ''}
+        onClick={onDone}
+      >
+        <div id="beamsLogo">
+          <div id="beamsProgressArea">
+            {Array.from(Array(bar).keys()).map((i) => (
+              <img src={beamsProgress} className="beamsProgress" key={i} />
+            ))}
+          </div>
         </div>
       </div>
+      <div id="splashFrameAntenna" />
+      <div id="splashFrameTop" />
+      <div id="splashFrameLeft" />
+      <div id="splashFrameRight" />
+      <div id="splashFrameBottom" />
+      <div id="splashFrameLT" />
+      <div id="splashFrameRT" />
+      <div id="splashFrameLB" />
+      <div id="splashFrameRB" />
     </div>
   )
 }
