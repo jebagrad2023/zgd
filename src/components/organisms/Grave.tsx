210d1e5190982f1c03dac3c43bbd0790fc6d1a83
Robonoaware <nhkonspiracist@gmail.com>
2023-05-13 15:17:17 -0700

    Show zea grave screenshots when memorial sign clicked

--- src/components/organisms/Grave.tsx
+++ src/components/organisms/Grave.tsx
@@ -1,9 +1,13 @@
 import React, { useEffect, useRef } from 'react'
 
 import { usePersistState } from '@zgd/hooks/usePersistState'
+import { ZoomableImage } from '@zgd/components/organisms/ZoomableImage'
 
 import MemorialFrame from '@zgd/images/zeaframewiggle1.gif'
 import zeaGhost from '@zgd/images/zea_ghost.png'
+import zeaGraveMarker from '@zgd/images/zea_grave_marker.png'
+import zeaGraveHer from '@zgd/images/zea_grave_her.png'
+import zeaGraveLivers from '@zgd/images/zea_grave_livers.png'
 
 export const Grave = (): JSX.Element => {
   const [clickedCnt, setClickedCnt] = usePersistState('GhostClickedCount', 0)
@@ -39,6 +43,20 @@ export const Grave = (): JSX.Element => {
     ghost.current.style.display = 'none'
   }
 
+  const showHideGrave = () => {
+    const images = document.querySelectorAll<HTMLElement>(
+      '.graveHer, .graveLivers',
+    )
+    const scrollContainer = document.getElementById('contents')
+    images.forEach((image) => {
+      const display = image.style.display === 'none' ? 'block' : 'none'
+      image.style.display = display
+      if (display === 'block') {
+        scrollContainer.scrollTop = scrollContainer.scrollHeight
+      }
+    })
+  }
+
   return (
     <div className="graveBG">
       <div className="graveContents">
@@ -47,7 +65,13 @@ export const Grave = (): JSX.Element => {
           ZEA CORNELIA <br />
         </div>
         <img src={MemorialFrame} className="frame" /> <br />
-        <div className="bottomMemorialTitle">2019 - 2023</div>
+        <img
+          className="bottomMemorialTitle"
+          src={zeaGraveMarker}
+          onClick={() => showHideGrave()}
+        />
+        <ZoomableImage className="graveLivers" src={zeaGraveLivers} />
+        <ZoomableImage className="graveHer" src={zeaGraveHer} />
         {clickedCnt > 0 && (
           <div id="zeaGhostClickCnt">YOU GOT {clickedCnt} GHOSTS</div>
         )}
