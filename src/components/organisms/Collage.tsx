e97dd578ceb32a08b374fe7f01e793db4a773d9f
Doramanjyu <24859285+Doramanjyu@users.noreply.github.com>
2023-05-13 15:21:58 +0900

    Add easter egg to collage page

--- src/components/organisms/Collage.tsx
+++ src/components/organisms/Collage.tsx
@@ -9,10 +9,21 @@ const playPauseClick = () => {
   })
 }
 
-export const Collage = (): JSX.Element => (
-  <>
+export const Collage = (): JSX.Element => {
+  const click = () => {
+    const confirmed = window.confirm(
+      'WARNING!!\nYou are entering to the [SECRET #ZEANGE ZONE].\nAre you a responsible adult?',
+    )
+    if (confirmed) {
+      window.open('https://youtu.be/8cOzNcHSDy4')
+    }
+  }
+  return (
     <div className="scrolling-image-container">
       <div className="scrolling-image" onClick={() => playPauseClick()}></div>
+      <div id="hiddenLink" className="cursorPointer" onClick={click}>
+        SECRET #ZEANGE ZONE
+      </div>
     </div>
-  </>
-)
+  )
+}
